3321fe208eeea80512839ceef6b614263d2f4750
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-07-25 09:30:29 -0300

    antes do link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,38 @@
-import styled from "styled-components";
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import { AlurakutMenu } from "../src/lib/AlurakutCommons";
 
-const Box = styled.div`
-  background: #ffffff;
-  border-radius: 8px;
-`;
-
-const MainGrid = styled.main`
-  width: 100%;
-  grid-gap: 10px;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 500px;
-  padding: 16px;
-  .profileArea {
-    display: none;
-    @media (min-width: 860px) {
-      display: block;
-    }
-  }
-  
-  @media (min-width: 860px) {
-    max-width: 1110px;
-    display: grid;
-    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
-    grid-template-columns: 160px 1fr 312px;
-  }
-`;
+function ProfileSideBar(props) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${props.githubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
+    </Box>
+  );
+}
 
 export default function Home() {
+  const githubUser = "ek1l";
   return (
-    <MainGrid>
-      <div className="profileArea" style={{ gridArea: "profileArea" }}>
-        <img src="https://github.com/ek1l.png" />
-      </div>
-      <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
-        <Box>Bem Vindo</Box>
-      </div>
-      <div
-        className="profileRelationsArea"
-        style={{ gridArea: "profileRelationsArea" }}
-      >
-        <Box>Pessoas da comunidade</Box>
-        <Box>Comunidades</Box>
-      </div>
-    </MainGrid>
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSideBar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>Bem Vindo</Box>
+        </div>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
+          <Box>Pessoas da comunidade</Box>
+          <Box>Comunidades</Box>
+        </div>
+      </MainGrid>
+    </>
   );
 }
